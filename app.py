dfc2b8dcccd377a11c7db1175bdcc324700f90f8
wangyushiyi <102666233+wangyushiyi@users.noreply.github.com>
2024-07-29 18:49:48 +0800

    Updated code for Vercel deployment

--- app.py
+++ app.py
@@ -4,56 +4,27 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Player(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    money = db.Column(db.Float, default=1000)
-    daily_income = db.Column(db.Float, default=100)
-    last_login = db.Column(db.DateTime, default=datetime.utcnow)
-    assets = db.Column(db.Integer, default=0)
-    companies = db.Column(db.Integer, default=0)
+# ... [rest of your existing code] ...
 
-@app.route('/')
-def index():
-    player = Player.query.first()
-    if not player:
-        player = Player()
-        db.session.add(player)
-        db.session.commit()
-    
-    current_time = datetime.utcnow()
-    time_diff = current_time - player.last_login
-    days_passed = time_diff.days
-    
-    if days_passed > 0:
-        player.money += player.daily_income * days_passed
-        player.last_login = current_time
-        db.session.commit()
-    
-    return render_template('index.html', player=player)
+# Add this at the end of the file
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
 
-@app.route('/buy_asset')
-def buy_asset():
-    player = Player.query.first()
-    if player.money >= 500:
-        player.money -= 500
-        player.assets += 1
-        player.daily_income += 10
-        db.session.commit()
-    return redirect(url_for('index'))
+def handler(event, context):
+    class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            body = BytesIO()
+            body.write(app.dispatch_request())
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(body.getvalue())
 
-@app.route('/create_company')
-def create_company():
-    player = Player.query.first()
-    if player.money >= 2000:
-        player.money -= 2000
-        player.companies += 1
-        player.daily_income += 50
-        db.session.commit()
-    return redirect(url_for('index'))
+    httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
+    return httpd.handle_request()
 
+# Keep this for local development
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
